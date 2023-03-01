2c8acda24c60841d6ac36f4d9858a5527836dcd4
hwimae <117029617+hwimae@users.noreply.github.com>
2023-03-01 13:37:49 +0700

    Add files via upload

---
+++ bài 30.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int x = n;
+    while (n != 0) {
+
+        for (int i = 0; i < n; i++) cout << '*'; for (int i = 0; i < x - n; i++) cout << ' ';
+        cout << endl;
+        n--;
+    }
+}
